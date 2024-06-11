3d91a63aa3b3f47d08b8a730f8c8c1c481991044
admin <aryanpatkar99@gmail.com>
2024-06-12 00:08:43 +0530

    added yt searching and downloading feature

--- spotify_api/spotify.py
+++ spotify_api/spotify.py
@@ -1,10 +1,7 @@
 import requests
 import refresh_token
-import json
-from pytube import YouTube
 
 token_ = "abcd"
-api_key="AIzaSyAsnsXx4oeGrWQcVKusU9j8et0VKTzbpQk"
 
 def get_playlists(token):
     headers = {
@@ -19,7 +16,7 @@ def get_tracks(token, playlist_id):
     }
     res = requests.get(f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks", headers=headers)
     return res
-name=[]
+
 def fun(token):
     res = get_playlists(token)
     try:
@@ -28,6 +25,7 @@ def fun(token):
             token = refresh_token.re_to()
             res = get_playlists(token)
             user = res.json()
+            print(user)
             playlist_id = user["items"][0]["id"] #add iteration for multiple playlists
             tracks_res = get_tracks(token, playlist_id)
             if tracks_res.status_code == 200:
@@ -36,30 +34,20 @@ def fun(token):
                 if tracks["items"]==mylist:
                     print("no tracks available")
                 else:
-                    
                     for i, result in enumerate(tracks["items"], 0):
-                        print(i, result["track"]["name"])
-                        name.append(result["track"]["name"])
+                        print(i, result["track"]["name"], result["track"]["id"])
             else:
                 print("Failed to get tracks:", tracks_res.status_code, tracks_res.text)
     except:
         print("no play lists found")
         return token
 
-token_ = fun(token_)
 
-#yt searching
-yt_url="https://www.googleapis.com/youtube/v3/search"
-yt_res=requests.get(yt_url,params={"key":api_key,"q":name[8],"type":"video"})#iterate through the song name is array of songs
-print(yt_res.status_code)
-yt_js=yt_res.json()
-video_id=yt_js["items"][0]["id"]["videoId"]#this part is video recomendation
+'''
+TO test if the code is running, run this file seperately. 
+To use the code, import this file
+'''
+if __name__ == "__main__":
+    token_ = fun(token_)
+    
 
-# downloading part
-print("download started.......")
-yt=YouTube(f'https://www.youtube.com/watch?v={video_id}')
-res=yt.streams.filter(only_audio=True,abr="128kbps")
-for tag in res:
-    print(tag.itag)
-stream = yt.streams.get_by_itag(tag.itag)
-stream.download(output_path="./donwloads")
